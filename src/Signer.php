c1f608bb6b8725173eb7de48a3019496fbb15f6f
tooru <tooru@tooru.uk>
2024-04-12 18:16:42 +0300

    Initial commit
    Co-Authored-By: Maksims Gerasimovs <maksimsge@gmail.com>

---
+++ src/Signer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BB;
+
+final readonly class Signer
+{
+    private const string ALGORITHM = 'sha512';
+
+    private function __construct()
+    {
+    }
+
+    public static function sign(array $data, #[\SensitiveParameter] string $key): array
+    {
+        $data['meta']['signature'] = self::hash($data, $key);
+
+        return $data;
+    }
+
+    public static function hash(array $data, #[\SensitiveParameter] string $key): string
+    {
+        unset($data['meta']['signature']);
+        self::rksort($data);
+
+        return hash_hmac(self::ALGORITHM, serialize($data), $key);
+    }
+
+    public static function validate(array $data, #[\SensitiveParameter] string $key): bool
+    {
+        return hash_equals(self::hash($data, $key), $data['meta']['signature'] ?? '');
+    }
+
+    /**
+     * Recursively sort array by keys in lexicographical order.
+     */
+    private static function rksort(array &$array): void
+    {
+        foreach ($array as &$value) {
+            if (is_array($value)) {
+                self::rksort($value);
+            }
+        }
+        ksort($array, SORT_STRING);
+    }
+}
