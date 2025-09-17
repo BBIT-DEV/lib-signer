33b0b20e87b4ed43ddd388834e35592900e4817e
microwave <maksimsge@gmail.com>
2025-09-17 10:28:46 +0300

    Update SignerTest.php

--- tests/SignerTest.php
+++ tests/SignerTest.php
@@ -14,7 +14,7 @@ class SignerTest extends \PHPUnit\Framework\TestCase
                 'signature' => 'e2a75e0f073ca4bb622e0d3c7dfe33a910c0beefe022dd00c85026fd2f1ab7150d30fd84a074b5b78a27c611fad45bcecb104ca0069f0398faa6ce9e989dbe30',
                 'request' => [
                     'meta' => [
-                        'payment_id' => 'someId2',
+                        'reference_id' => 'someId2',
                         'service_id' => 1231,
                         'signature' => 'some_generated_signature2'
                     ],
@@ -35,7 +35,7 @@ class SignerTest extends \PHPUnit\Framework\TestCase
                 'signature' => 'c7e484ff4a6260c91578ea2bf23c0a80aa870d3fb671fc8ca349c8e8ea8fe38fe47aca529062662a191152b94b94df62b9dbb779d023b11855c15aeaa22533f0',
                 'request' => [
                     'meta' => [
-                        'payment_id' => 'someId',
+                        'reference_id' => 'someId',
                         'service_id' => 1231,
                         'signature' => 'some_generated_signature'
                     ],
@@ -56,7 +56,7 @@ class SignerTest extends \PHPUnit\Framework\TestCase
                 'signature' => 'dff11096fc79fe3f05db2a6d22697f2250496fbd02d52ad4f22e395a231f3e7ec5f4a9ddc4ab8b1333758a008976d7a3907b07ec49002758477ec9a9bbbfbc61',
                 'request' => [
                     'meta' => [
-                        'payment_id' => 'someId2',
+                        'reference_id' => 'someId2',
                         'service_id' => 1231,
                         'signature' => 'some_generated_signature2'
                     ],
